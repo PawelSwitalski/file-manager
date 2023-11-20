63b6cb71f6e04be58041cf5d0504e904607899e6
PawelSwitalski <paswitalski@gmail.com>
2023-11-20 19:00:26 +0100

    Change AuthenticatedLayout.vue Create components Navigation.vue, SearchForm.vue, UserSettingsDropdown.vue, CreateNewDropdown.vue

---
+++ resources/js/Components/app/SearchForm.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        SearchForm
+    </div>
+</template>
+
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/app/UserSettingsDropdown.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        UserSettingsDropdown
+    </div>
+</template>
+
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
