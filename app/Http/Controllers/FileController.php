3c6e0b1ef8d9365fdbd0f1780712b815e4fab9da
PawelSwitalski <paswitalski@gmail.com>
2023-11-27 13:02:26 +0100

    add FileController and modify RegisteredUserController.

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFolderRequest;
+use App\Models\File;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class FileController extends Controller
+{
+    public function myFiles()
+    {
+        return Inertia::render('MyFiles');
+    }
+
+    public function createFolder(StoreFolderRequest $request)
+    {
+        $data = $request->validated();
+        $parent = $request->parent;
+
+        if (!$parent) {
+            $parent = $this->createRootFolder();
+        }
+
+        $folder = File::create([
+            'name' => $data['name'],
+            'is_folder' => true,
+        ]);
+
+        $parent->appendNode($folder);
+
+//        return Inertia::render('CreateFile');
+    }
+
+    private function createRootFolder()
+    {
+        return File::query()->whereIsRoot()->where('created_by', Auth::id())->firstOrFail();
+    }
+}
