53ed372234ea12c3f8a81b4168ee0829638b87ff
PawelSwitalski <paswitalski@gmail.com>
2023-11-27 12:47:02 +0100

    modify Navigation.vue and create MyFiles.vue

--- resources/js/Components/app/Navigation.vue
+++ resources/js/Components/app/Navigation.vue
@@ -1,16 +1,15 @@
 <template>
     <nav class="min-w-[200px]">
-        <div class="h-[80px] px-3 flex items-center gap-3">
-            <Link href="/">
-                <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
-            </Link>
+        <Link class="h-[80px] px-3 flex items-center gap-3" :href="route('files.index')">
+            <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
             LaraStorage
-        </div>
+        </Link>
+
         <div class="px-3">
             <CreateNewDropdown />
 
             <div class="py-3">
-                <NavLink href="/">My Files</NavLink>
+                <NavLink :href="route('files.index')" :active="$page.url === '/files'">My Files</NavLink>
                 <NavLink href="/">Shared with me</NavLink>
                 <NavLink href="/">Shared by me</NavLink>
                 <NavLink href="/">Trash</NavLink>
