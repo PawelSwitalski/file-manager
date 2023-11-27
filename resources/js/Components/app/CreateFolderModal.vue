4cb771bf761ec65b4a964e7a38e5e4ebe86afeb9
PawelSwitalski <paswitalski@gmail.com>
2023-11-27 15:11:22 +0100

    CreateFolderModal.vue and changes in CreateNewDropdown.vue.

---
+++ resources/js/Components/app/CreateFolderModal.vue
@@ -0,0 +1,99 @@
+<template>
+    <modal :show="modelValue" @show="onShow" max-width="sm">
+        <div class="p-6">
+            <h2 class="text-lg font-medium text-gray-900">
+                Crate New Folder
+            </h2>
+            <div class="mt-6">
+                <InputLabel for="folderName" value="Folder Name" class="sr-only"/>
+                <TextInput type="text"
+                           ref="folderNameInput"
+                           id="folderName" v-model="form.name"
+                           class="mt-1 block w-full"
+                           :class="form.errors.name ? 'border-red-500 focus:border-red-500 focus:ring-red-500' :''"
+                           placeholder="Folder Name"
+                           @keyup.enter="createFolder"
+                />
+
+                <InputError :message="form.errors.name" class="mt-2"/>
+            </div>
+            <div class="mt-6 flex justify-end">
+                <SecondaryButton @click="closeModal">Cancel</SecondaryButton>
+                <PrimaryButton class="ml-3"
+                               :class="{ 'opacity-25': form.processing }"
+                               @click="createFolder" :disable="form.processing">
+                    Submit
+                </PrimaryButton>
+            </div>
+        </div>
+    </modal>
+</template>
+
+<script setup>
+// Imports
+import Modal from "@/Components/Modal.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+import InputError from "@/Components/InputError.vue";
+import {useForm} from "@inertiajs/vue3";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import {nextTick, ref} from "vue";
+
+// Props & emits
+const {modelValue} = defineProps({
+    modelValue: Boolean
+});
+const emit = defineEmits(['update:modelValue']);
+
+// Uses
+const form = useForm({
+    name: ''
+});
+
+// Refs
+const folderNameInput = ref(null);
+
+// Methods
+function onShow() {
+    nextTick(() => {
+        folderNameInput.value.focus();
+    });
+}
+
+function createFolder() {
+    console.log('create folder');
+    form.post(route('folder.create'), {
+        preserveScroll: true,
+        onSuccess: () => {
+            closeModal()
+            form.reset();
+        },
+        onError: () => {
+            folderNameInput.value.focus();
+        }
+    })
+    // axios.post('/folders', form)
+    //     .then(({data}) => {
+    //         $dispatch('folderCreated', data);
+    //         $dispatch('closeCreateFolderModal');
+    //     })
+    //     .catch(({response}) => {
+    //         if (response.status === 422) {
+    //             form.errors = response.data.errors;
+    //         }
+    //     });
+}
+
+function closeModal() {
+    emit('update:modelValue');
+    form.clearErrors();
+    form.reset();
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
--- resources/js/Components/app/CreateNewDropdown.vue
+++ resources/js/Components/app/CreateNewDropdown.vue
@@ -21,7 +21,7 @@
             >
                 <div class="px-1 py-1">
                     <MenuItem v-slot="{ active }">
-                        <a class="text-gray-700 block px-4 py-2 text-sm">
+                        <a href="#" @click.prevent="showCreateFolderModal" class="text-gray-700 block px-4 py-2 text-sm">
                             New Folder
                         </a>
                     </MenuItem>
@@ -43,12 +43,22 @@
             </MenuItems>
         </transition>
     </Menu>
-
+    <CreateFolderModal v-model="createFolderModal"/>
 </template>
 
 <script setup>
-
+// Imports
 import {Menu, MenuButton, MenuItem, MenuItems} from "@headlessui/vue";
+import CreateFolderModal from "@/Components/app/CreateFolderModal.vue";
+import {ref} from "vue";
+
+//Refs
+const createFolderModal = ref(false);
+
+// Methods
+function showCreateFolderModal() {
+    createFolderModal.value = true;
+}
 </script>
 
 <style scoped>
