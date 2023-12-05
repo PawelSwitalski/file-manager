a11572a7ecb6875d0955b52fb84c9a47df8cefde
PawelSwitalski <paswitalski@gmail.com>
2023-12-05 13:08:08 +0100

    add icons

---
+++ resources/js/Helper/file-helper.js
@@ -0,0 +1,89 @@
+export function isImage(file) {
+    return /^image\/\w+$/.test(file.mime)
+}
+
+export function isPDF(file) {
+    return [
+        'application/pdf',
+        'application/x-pdf',
+        'application/acrobat',
+        'application/vnd.pdf',
+        'text/pdf',
+        'text/x-pdf',
+    ].includes(file.mime)
+}
+
+export function isAudio(file) {
+    return [
+        'audio/mpeg',
+        'audio/ogg',
+        'audio/wav',
+        'audio/x-m4a',
+        'audio/webm',
+    ].includes(file.mime)
+}
+
+export function isVideo(file) {
+    return [
+        'video/mp4',
+        'video/mpeg',
+        'video/ogg',
+        'video/quicktime',
+        'video/webm',
+    ].includes(file.mime)
+}
+
+export function isWord(file) {
+    return [
+        'application/msword',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'application/vnd.ms-word.document.macroEnabled.12',
+        'application/vnd.ms-word.template.macroEnabled.12',
+    ].includes(file.mime)
+}
+
+export function isExcel(file) {
+    return [
+        'application/vnd.ms-excel',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'application/vnd.ms-excel.sheet.macroEnabled.12',
+        'application/vnd.ms-excel.template.macroEnabled.12',
+    ].includes(file.mime)
+}
+
+export function isZip(file) {
+    return [
+        'application/zip',
+    ].includes(file.mime)
+}
+
+export function isText(file) {
+    return [
+        'text/plain',
+        'text/html',
+        'text/css',
+        'text/javascript',
+        'text/csv',
+    ].includes(file.mime)
+}
+
+export function isExecutable(file) {
+    return [
+        'application/x-msdownload',
+        'application/x-ms-installer',
+        'application/x-ms-dos-executable',
+        'application/x-executable',
+        'application/x-exe',
+        'application/x-ms-exe',
+        'text/exe',
+    ].includes(file.mime)
+}
+
+export function isPowerPoint(file) {
+    return [
+        'application/vnd.ms-powerpoint',
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+        'application/vnd.ms-powerpoint.template.macroEnabled.12',
+    ].includes(file.mime)
+}
--- resources/js/Pages/MyFiles.vue
+++ resources/js/Pages/MyFiles.vue
@@ -45,7 +45,8 @@
             <tr v-for="file of files.data" :key="file.id"
                 @dblclick="openFolder(file)"
                 class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100 cursor-pointer">
-                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 flex items-center">
+                    <FileIcon :file="file"></FileIcon>
                     {{ file.name }}
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
@@ -72,6 +73,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import {router} from "@inertiajs/vue3";
 import {Link} from "@inertiajs/vue3";
+import FileIcon from "@/Components/app/FileIcon.vue";
 
 // Uses
 
